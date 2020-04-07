2d0a56018402b0063a51e3ff82f29ce9cfdd8cf2
probe2k <kyash054@gmail.com>
2020-04-07 20:24:37 +0530

    Initial Commit

---
+++ precache-manifest.28a88b07212e99229acc3a61fe90dfcc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76481d71b3659293e4dbe82f90019d6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf536899295b443db05c",
+    "url": "/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "e4e89e14f279e1d87630",
+    "url": "/static/css/main.a4a4f781.chunk.css"
+  },
+  {
+    "revision": "cf536899295b443db05c",
+    "url": "/static/js/2.dafaff02.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/static/js/2.dafaff02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4e89e14f279e1d87630",
+    "url": "/static/js/main.39eb6903.chunk.js"
+  },
+  {
+    "revision": "caf2a04453df4bace4e4",
+    "url": "/static/js/runtime-main.009173a0.js"
+  },
+  {
+    "revision": "1406e9733dd1f17c7b6ca6d33ec2498b",
+    "url": "/static/media/profile.1406e973.png"
+  },
+  {
+    "revision": "6ecc59877830875b3ca71b87d9978ee4",
+    "url": "/static/media/resume.6ecc5987.pdf"
+  }
+]);
