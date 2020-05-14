aa32fc447303aac4279ca7495234e98dcd12774a
probe2k <kyash054@gmail.com>
2020-05-15 02:04:38 +0530

    Resume fix

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-a5803e1d876ccec33929.js"
+    "url": "webpack-runtime-93ddc2e6a78d3d7ac567.js"
   },
   {
     "url": "framework-f7f9b643de0d3b0e5da1.js"
@@ -39,7 +39,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "60f31636b6149eb1c18dcb1f6cc0dd00"
+    "revision": "930f830d7300d7450f7476884af32677"
   },
   {
     "url": "manifest.json",
